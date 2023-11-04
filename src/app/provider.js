dddcb83ab57a1624a4dd34bcabb26d8e4be40c26
RAYMUND LAPUZ <111259553+Rlapuz@users.noreply.github.com>
2023-11-05 02:04:41 +0800

    Skeleton (#4)
    * Initial commit from Create Next App
    
    * first commit
    
    * first commit

---
+++ src/app/provider.js
@@ -0,0 +1,13 @@
+'use client'
+
+import { SessionProvider } from 'next-auth/react'
+
+
+export const NextAuthProvider = ({ children }) => {
+    return (
+        <SessionProvider>
+            {children}
+        </SessionProvider>
+    )
+
+}
