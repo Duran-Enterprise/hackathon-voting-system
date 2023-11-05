3197af723c9ee57c711e710a6ae9f82bc77683d4
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-11-05 10:47:06 +0800

    Fix external babel issues on build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        forceSwcTransforms: true,
+    },
+};
 
 module.exports = nextConfig;
