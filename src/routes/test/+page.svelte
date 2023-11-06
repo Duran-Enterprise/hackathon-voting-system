d86269b2f3c185ecd8e26609742e2cf0f035e257
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-11-07 04:03:20 +0800

    #23 #24 Add test route to prevent discord timeouts

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Modals } from '@/types/index';
+	import CreatePollModal from '@components/CreatePollModal.svelte';
+	import VotePollModal from '@components/VotePollModal.svelte';
+	import { openModal } from '@utils/index';
+	const createPoll = Modals.CreatePoll;
+	const votePoll = Modals.VotePoll;
+</script>
+
+<h1>Test</h1>
+<button class="btn mt-4" on:click={() => openModal(createPoll)}>Create Poll</button>
+<CreatePollModal />
+<hr />
+<button class="btn mt-4" on:click={() => openModal(votePoll)}>Vote Poll</button>
+<VotePollModal />
+<hr />
