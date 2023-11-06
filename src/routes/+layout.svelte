46dd3c99eb151d5cb796a95767632641f2be4bf9
Denver James Duran <denverjames.duran19@outlook.com>
2023-11-06 15:54:52 +0800

    #18 Add meta description and title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
 	import '$lib/styles/app.css';
 </script>
 
+<svelte:head>
+	<title>Cast Away - Voting webapp for Daedalus</title>
+	<meta name="description" content="Voting webapp exclusive for Daedalus discord members" />
+</svelte:head>
+
 <slot />
