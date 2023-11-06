93f8908e196b3dd6252a349a5e5f33f5c143151e
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-11-07 07:17:03 +0800

    REMOVE PLAYWRIGHT

--- prepare.sh
+++ prepare.sh
@@ -3,4 +3,6 @@ clear
 echo -e "This script is brought to you by: "
 echo -e "\n\n\e[32mDenver James Duran\033[0m\n\n"
 
-npm run format && npm run lint && npm run check && npm run test
+npm run format && npm run lint && npm run check
+npx playwright test
+npm run test
