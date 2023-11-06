ccff94971d0ff7c2e6ea52c0e3581fbbf3b5bd69
Denver James Duran <denverjames.duran19@outlook.com>
2023-11-06 16:03:23 +0800

    #18 Update to have sensible navlinks

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -1,10 +1,14 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+</script>
+
 <div class="bg-gray-950 p-4 flex justify-between items-center">
-	<div class="text-2xl font-semibold text-white">Cast Away</div>
+	<button class="text-2xl font-semibold text-white" on:click={() => goto('/')}> Cast Away</button>
 
 	<div class="space-x-4">
-		<a href="/events/event1" class="text-white">Event 1</a>
-		<a href="/events/event2" class="text-white">Event 2</a>
-		<a href="/events/event3" class="text-white">Event 3</a>
+		<a href="/home" class="text-white">Home</a>
+		<a href="/polls" class="text-white">Polls</a>
+		<a href="/results" class="text-white">Results</a>
 	</div>
 
 	<a href="/login" class="text-white font-semibold">Login</a>
