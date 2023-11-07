164b2cd7e3fb74f31cc6929dc9147496ba787d75
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-11-08 01:34:06 +0800

    Add Types to mongoDB

---
+++ src/lib/server/schema/index.ts
@@ -0,0 +1,36 @@
+import type { ObjectId } from 'mongodb';
+
+export type UsernameDB = string;
+
+export type userDBSchema = {
+	_id: ObjectId;
+	username: UsernameDB;
+	active: boolean;
+};
+
+export type newUser = Partial<Omit<userDBSchema, '_id'>>;
+
+export type ChoiceDB = {
+	choice: string;
+	voters: UsernameDB[];
+};
+
+export type pollDBSchema = {
+	_id: ObjectId;
+	pollDescription: string;
+	choices: ChoiceDB[];
+	startDate: Date;
+	endDate: Date;
+};
+
+export type newPoll = Partial<Omit<pollDBSchema, '_id'>> & {
+	choices: ChoiceDB[] & { length: 2 };
+};
+
+export type newVote = Omit<
+	pollDBSchema,
+	'pollDescription' | 'startDate' | 'endDate' | 'choices'
+> & {
+	choice: string;
+	voter: string;
+};
