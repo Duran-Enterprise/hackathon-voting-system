722156f676412e2707922607fada51b071bafdd1
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-11-08 02:54:15 +0800

    Refine Zod

--- src/lib/server/middlewares/ZodSchemas.ts
+++ src/lib/server/middlewares/ZodSchemas.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { ObjectId } from 'mongodb';
 
-const ObjectIdOrString = z.union([z.string(), z.instanceof(ObjectId)]);
+export const ObjectIdOrString = z.union([z.string(), z.instanceof(ObjectId)]);
 
 export const userDBZod = z.object({
 	_id: ObjectIdOrString,
@@ -16,19 +16,33 @@ export const choiceZod = z.object({
 	voters: z.array(z.string())
 });
 
-export const pollDBZod = z.object({
-	_id: ObjectIdOrString,
-	pollDescription: z.string(),
-	choices: z.array(choiceZod),
-	startDate: z.date(),
-	endDate: z.date()
-});
-export const newPollZod = pollDBZod
-	.omit({ _id: true })
-	.partial()
-	.extend({
-		choices: z.array(choiceZod).min(2)
-	});
+const pastDate = z.date().refine(
+	(value) => {
+		const today = new Date();
+		return value >= today;
+	},
+	{
+		message: "Start date can't be in the past"
+	}
+);
+
+export const newPollZod = z
+	.object({
+		pollDescription: z
+			.string()
+			.min(5, { message: 'Poll description must be at least 5 characters long' }),
+		choices: z.array(choiceZod).min(2, { message: 'Must have at least two choices' }),
+		startDate: pastDate,
+		endDate: z.date()
+	})
+	.refine(
+		({ startDate, endDate }) => {
+			return endDate >= startDate;
+		},
+		{
+			message: 'End date must be after start date'
+		}
+	);
 
 export const newVoteZod = z.object({
 	_id: ObjectIdOrString,
