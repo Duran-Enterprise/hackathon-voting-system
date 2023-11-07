352f365b1c9b65f0bc5361a24e055565db4bd418
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-11-08 03:16:40 +0800

    Add docs

--- src/lib/server/middlewares/ErrorGenerator.ts
+++ src/lib/server/middlewares/ErrorGenerator.ts
@@ -1,6 +1,13 @@
 import type { ZodErrorResponse } from '@/types/index';
 
-export function errorGenerator(error: Error) {
+/**
+ * Generate a ZodErrorResponse from an error object or return the original error.
+ *
+ * @param {unknown} error - The error object to be transformed.
+ *
+ * @returns {ZodErrorResponse | unknown} An object containing an array of error details, where each detail is represented as an object with key-value pairs, or the original error if it's not an instance of Error.
+ */
+export function errorGenerator(error: unknown): ZodErrorResponse | unknown {
 	if (error instanceof Error) {
 		const errors: ZodErrorResponse = { errors: [] };
 		const errorData = JSON.parse(error.message);
@@ -14,4 +21,5 @@ export function errorGenerator(error: Error) {
 
 		return errors;
 	}
+	return error;
 }
