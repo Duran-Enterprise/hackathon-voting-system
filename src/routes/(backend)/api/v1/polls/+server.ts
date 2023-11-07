ef85b96a9e85af1fe785e6dee3b939ccacc4eae5
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-11-08 03:45:06 +0800

    Add POST /api/v1/polls for creating new poll

--- src/routes/(backend)/api/v1/polls/+server.ts
+++ src/routes/(backend)/api/v1/polls/+server.ts
@@ -1,4 +1,6 @@
+import { validateNewPoll } from '@server/middlewares/ZodValidator';
 import { Polls } from '@server/mongoDb/MongoDBProvider';
+import type { newPoll } from '@server/schema';
 import { json, type RequestHandler } from '@sveltejs/kit';
 import { responseGenerator } from '@utils/index';
 
@@ -6,4 +8,30 @@ export const GET: RequestHandler = async () => {
 	const polls = await Polls.findAllPolls();
 
 	return json(responseGenerator(polls));
+};
+export const POST: RequestHandler = async ({ request }) => {
+	const { pollDescription, choices, startDate, endDate } = await request.json().catch((error) => {
+		return json(
+			responseGenerator({
+				status: 'fail',
+				message: 'something went wrong',
+				error
+			})
+		);
+	});
+	const parsedStartDate = new Date(startDate);
+	const parsedEndDate = new Date(endDate);
+	const newPoll: newPoll = {
+		pollDescription,
+		choices,
+		startDate: parsedStartDate,
+		endDate: parsedEndDate
+	};
+
+	const validatedPoll = validateNewPoll(newPoll);
+	if (validatedPoll.status === 'fail') {
+		return json(validatedPoll);
+	}
+	const result = await Polls.insertPoll(newPoll);
+	return json(result);
 };
