08a4fbd580d373b906c26452d3247ca26358d8d8
Denver James Duran <denverjames.duran19@outlook.com>
2023-11-08 13:45:47 +0800

    Add methods for accessing users table
    GET: sends out all users
    PATCH: Updates user active status (default: true)
    DELETE: Updates user active status to false

--- src/routes/(backend)/api/v1/users/[_id]/+server.ts
+++ src/routes/(backend)/api/v1/users/[_id]/+server.ts
@@ -3,6 +3,11 @@ import { Users } from '@server/mongoDb/MongoDBProvider';
 import { json, type RequestHandler } from '@sveltejs/kit';
 import { responseGenerator } from '@utils/index';
 
+/**
+ *
+ *  Retrieves a user with the given ID.
+ *
+ * */
 export const GET: RequestHandler = async ({ params }) => {
 	const { _id } = params;
 	const validated_id = validateId(_id);
@@ -15,5 +20,50 @@ export const GET: RequestHandler = async ({ params }) => {
 		);
 	}
 	const user = await Users.findUser({ _id: validated_id });
+	return json(user);
+};
+
+/**
+ * Updates the status of a user with the given ID to false.
+ *
+ * */
+export const DELETE: RequestHandler = async ({ params }) => {
+	const { _id } = params;
+	const validated_id = validateId(_id);
+
+	if (!validated_id) {
+		return json(
+			responseGenerator({
+				status: 'fail',
+				message: 'invalid id'
+			})
+		);
+	}
+	const user = await Users.updateStatus(validated_id, false);
+	return json(user);
+};
+
+/**
+ * Updates the status of a user with the given ID.
+ *
+ * Defaults to setting active status of user to true, unless `active` query parameter is set to `false`.
+ */
+export const PATCH: RequestHandler = async ({ params, url }) => {
+	const { _id } = params;
+	const validated_id = validateId(_id);
+	let active = true;
+	if (url.searchParams.get('active') === 'false') {
+		active = false;
+	}
+
+	if (!validated_id) {
+		return json(
+			responseGenerator({
+				status: 'fail',
+				message: 'invalid id'
+			})
+		);
+	}
+	const user = await Users.updateStatus(validated_id, active);
 	return json(user);
 };
