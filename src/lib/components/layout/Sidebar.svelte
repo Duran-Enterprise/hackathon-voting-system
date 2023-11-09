6518cb177684e4c5de1c0bdd4941583f4aad627f
Denver James Duran <denverjames.duran19@outlook.com>
2023-11-10 07:55:06 +0800

    Fix sidebar theme

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -1,21 +1,39 @@
 <script lang="ts">
 	import type { DiscordUser } from '@/types/index';
 	import { getAvatarLink } from '@utils/index';
+	import home from '$lib/assets/home.svg';
+	import polls from '$lib/assets/polls.svg';
+	import results from '$lib/assets/results.svg';
+	import logout from '$lib/assets/logout.svg';
 	export let user: DiscordUser;
 </script>
 
 <ul class="menu p-4 min-h-full bg-darkBlack text-base-content">
-	<h1 class="my-5 text-center">Cast Away</h1>
+	<h1 class="my-5 text-center text-lightWhite">Cast Away</h1>
 	<div class="avatar my-4">
 		<div class="w-24 rounded mx-auto">
 			<img src={getAvatarLink(user)} alt="Discord Avatar" />
 		</div>
 	</div>
-	<h4 class="text-center uppercase mb-4">{user.username}</h4>
+	<h4 class="text-center uppercase mb-4 text-lightWhite">{user.username}</h4>
 
-	<li class="text-right text-lg"><a href="/home">Home</a></li>
-	<li class="text-right text-lg"><a href="/polls">Polls</a></li>
-	<li class="text-right text-lg"><a href="/results">Results</a></li>
+	<li>
+		<a class="text-lg gap-4 text-lightWhite" href="/home"><img src={home} alt="home icon" />Home</a>
+	</li>
+	<li>
+		<a class="text-lg gap-4 text-lightWhite" href="/polls"
+			><img src={polls} alt="polls icon" />Polls</a
+		>
+	</li>
+	<li>
+		<a class="text-lg gap-4 text-lightWhite" href="/results"
+			><img src={results} alt="results icon" />Results</a
+		>
+	</li>
 	<div class="divider my-4" />
-	<li class="text-right text-lg"><a href="/?logout=true">Logout</a></li>
+	<li>
+		<a class="text-lg gap-4 text-red-600" href="/?logout=true"
+			><img src={logout} alt="logout icon" />Logout</a
+		>
+	</li>
 </ul>
--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -2,16 +2,19 @@
 	import Sidebar from '@components/layout/Sidebar.svelte';
 	import type { LayoutServerData } from './$types';
 	import { AppBar, AppLayout } from 'svelte-ux';
+	import { page } from '$app/stores';
 	export let data: LayoutServerData;
 	let user = data.verifiedUser;
+	// get current route
+	$: route = $page.url.pathname;
 </script>
 
-<AppLayout>
+<AppLayout headerPosition="inset">
 	<nav slot="nav" class="h-full">
 		<Sidebar {user} />
 	</nav>
 
-	<AppBar title="Cast Away">
+	<AppBar title={route.slice(1).charAt(0).toUpperCase() + route.slice(2).toLowerCase()}>
 		<div slot="actions">
 			<!-- App actions -->
 		</div>
