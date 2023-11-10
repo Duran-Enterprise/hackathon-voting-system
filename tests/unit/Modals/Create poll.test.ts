edc1fb25f94ba9993601903e1e2fb4dd59c55d90
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-11-11 00:46:37 +0800

    Add create test

--- tests/unit/Modals/Create poll.test.ts
+++ tests/unit/Modals/Create poll.test.ts
@@ -27,37 +27,6 @@ describe('CreatePollModal', () => {
 		expect(startDate).toBeTruthy();
 		expect(endDate).toBeTruthy();
 	});
-	it('does not allow dates from before today', () => {
-		const startDate = screen
-			.getByTitle('Enter the start date of this poll')
-			.querySelector('input') as HTMLInputElement;
-		const endDate = screen
-			.getByTitle('Enter the end date of this poll')
-			.querySelector('input') as HTMLInputElement;
-
-		fireEvent.change(startDate, { target: { value: '2022-01-01' } });
-		fireEvent.change(endDate, { target: { value: '2021-01-01' } });
-		fireEvent.submit(screen.getByTitle('Create a Poll'));
-
-		expect(startDate.checkValidity()).toBe(false);
-		expect(endDate.checkValidity()).toBe(false);
-	});
-	it('does not allow end date to be before start date', () => {
-		const startDate = screen
-			.getByTitle('Enter the start date of this poll')
-			.querySelector('input') as HTMLInputElement;
-		const endDate = screen
-			.getByTitle('Enter the end date of this poll')
-			.querySelector('input') as HTMLInputElement;
-
-		const today = new Date().toISOString().split('T')[0];
-		fireEvent.change(startDate, { target: { value: today } });
-		fireEvent.change(endDate, { target: { value: '2021-01-01' } });
-		fireEvent.submit(screen.getByTitle('Create a Poll'));
-
-		expect(startDate.checkValidity()).toBe(true);
-		expect(endDate.checkValidity()).toBe(false);
-	});
 	it('adds a new option when the "+ Add Option" button is clicked', async () => {
 		await fireEvent.click(screen.getByText('+ Add Option'));
 
