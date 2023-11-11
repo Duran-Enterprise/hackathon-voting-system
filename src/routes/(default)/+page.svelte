54855b87a58367652a98997549577d8fa42f73a2
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-11-11 12:38:26 +0800

    Create a simple landing page

--- src/routes/(default)/+page.svelte
+++ src/routes/(default)/+page.svelte
@@ -1,1 +1,4 @@
-<h1>Landing</h1>
+<div class="flex flex-col text-center h-[80vh] justify-center gap-4">
+	<h1 class="text-balance">Cast Away - Hackathon Voting System</h1>
+	<a href="/login">Login to get started</a>
+</div>
